5dd4ece88ceebb6e64755425b27ce24abe4725a8
zaham2 <zaham2@yahoo.com>
2019-09-19 07:15:27 +0200

    Added Blog model.

--- src/cv/settings.py
+++ src/cv/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
